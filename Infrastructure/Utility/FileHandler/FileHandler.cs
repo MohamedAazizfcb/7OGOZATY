835fc42cf757d67d374b53af7f9bf39bd1dfa4b0
MohamedAazizfcb <mohamedaazizfcb@gmail.com>
2025-02-14 03:15:59 +0200

    image fix

--- Infrastructure/Utility/FileHandler/FileHandler.cs
+++ Infrastructure/Utility/FileHandler/FileHandler.cs
@@ -27,7 +27,7 @@ namespace Infrastructure.Utility.FileHandler
                 throw new InvalidOperationException("Error while uploading the file.", ex);
             }
 
-            return path;
+            return path.Replace(AppConstants.wwwroot + '/', "");
         }
 
         public void Delete(string filePath)
